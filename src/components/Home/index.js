c91a9f90db3ea448caa10d6e83edb0de7e847958
saikumar3570 <saikumar3570@gmail.com>
2023-03-26 12:11:30 +0000

    solution

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,9 @@
 import {Component} from 'react'
-import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom'
 import Header from '../Header/index'
 // import ThemeContext from '../../context/context'
 
 class Home extends Component {
   render() {
-    const jwt = Cookies.get('jwt_token')
-    if (jwt === undefined) {
-      return <Redirect to="/login" />
-    }
     return (
       <div>
         <Header />
